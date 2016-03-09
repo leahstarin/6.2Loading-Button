632df05084eead0f445a568a4b964a8d2c64cf62
Leah Starin <leahstarin@Leah-Starins-MacBook-Pro.local>
2016-03-09 09:08:42 -0500

    final submit

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,1 +1,22 @@
-console.log("Hello World!");
+var $ = require('jquery');
+var Backbone = require('backbone');
+var models = require('./models/planets');
+
+console.log(models);
+
+var starWarsPlanet = new models.PlanetCollection();
+
+
+var planet = new models.Planet();
+
+
+
+$('button').click(function(){
+$('.buttonLabel').html("Loading...")
+starWarsPlanet.fetch().done(function(){
+  starWarsPlanet.each(function(planet){
+    console.log(planet.get('name'));
+    $('.buttonLabel').html("Submit")
+    });
+  });
+});
---
+++ app/scripts/models/planets.js
@@ -0,0 +1,29 @@
+var Backbone = require('backbone');
+var $ = require('jquery');
+
+
+var Planet = Backbone.Model.extend({
+  // defaults: {
+  //   "buttonLabel": "Submit"
+  // },
+  // initialize: function(){
+  //   $('.buttonLabel').text('Loading...');
+  // }
+
+});
+
+
+var PlanetCollection = Backbone.Collection.extend({
+  model: Planet,
+  url: 'http://swapi.co/api/planets/',
+  parse: function(data){
+    return data.results;
+    console.log(PlanetCollection)
+  }
+});
+
+module.exports = {
+  "Planet" : Planet,
+  "PlanetCollection" : PlanetCollection
+
+}
